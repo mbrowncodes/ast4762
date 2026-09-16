8f38d2dd348d763ec4522c82229b7125c4d18704
mbrowncodes <ma755497@ucf.edu>
2026-09-16 01:02:57 -0400

    Homework 3 Solving and Revision

--- homework/hw3_kenzb4/hw3_kenzb4_support_module.py
+++ homework/hw3_kenzb4/hw3_kenzb4_support_module.py
@@ -130,12 +130,17 @@ def squareplot(low, high, pt_num, saveplot=False):
 
     Examples
     --------
-    """
     >>>squareplot(0, 10, 5)
+    
+    """
+    
 
     
     x = np.linspace(low, high, pt_num) #makes array from input graph values
+    #includes highest value in x
+    print(x)
     y = square(x) #makes new array that is square of x array values
+    print(y)
 
     plt.figure(  figsize=(8,5)  ) #make figure readable size
     plt.plot(x, y)  # plot x vs y 
@@ -146,122 +151,13 @@ def squareplot(low, high, pt_num, saveplot=False):
     #saveplot = False
     return
 
-   
 
-#example function 
-def foo(var1, var2, long_var_name='hi') :
-    #this is our function's docstring!
-    """A one-line summary that does not use variable names or the
-    function name.
-
-    Several sentences providing an extended description. Refer to
-    variables using back-ticks, e.g. `var`.
-
-    Parameters
-    ----------
-    var1 : array_like
-        Array_like means all those objects -- lists, nested lists, etc. --
-        that can be converted to an array.  We can also refer to
-        variables like `var1`.
-    var2 : int
-        The type above can either refer to an actual Python type
-        (e.g. ``int``), or describe the type of the variable in more
-        detail, e.g. ``(N,) ndarray`` or ``array_like``.
-    Long_variable_name : {'hi', 'ho'}, optional
-        Choices in brackets, default first when optional.
-
-    Returns
-    -------
-    describe : type
-        Explanation
-    output : type
-        Explanation
-    tuple : type
-        Explanation
-    items : type
-        even more explaining
-
-    Other Parameters
-    ----------------
-    only_seldom_used_keywords : type
-        Explanation
-    common_parameters_listed_above : type
-        Explanation
-
-    Raises
-    ------
-    BadException
-        Because you shouldn't have done that.
-
-    See Also
-    --------
-    otherfunc : relationship (optional)
-    newfunc : Relationship (optional), which could be fairly long, in which
-              case the line wraps here.
-    thirdfunc, fourthfunc, fifthfunc
-
-    Notes
-    -----
-    Notes about the implementation algorithm (if needed).
-
-    This can have multiple paragraphs.
-
-    You may include some math:
-
-    .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
-
-    And even use a greek symbol like :math:`omega` inline.
-
-    References
-    ----------
-    Cite the relevant literature, e.g. [1]_.  You may also cite these
-    references in the notes section above.
-
-    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
-       expert systems and adaptive co-kriging for environmental habitat
-       modelling of the Highland Haggis using object-oriented, fuzzy-logic
-       and neural-network techniques," Computers & Geosciences, vol. 22,
-       pp. 585-588, 1996.
-
-    Examples
-    --------
-    These are written in doctest format, and should illustrate how to
-    use the function.  Use the plain python prompt (">>> ") for the
-    example commands, and put the expected output on a line without a
-    prompt, as follows:
-
-
-    >>> a=[1,2,3]
-    >>> print([x + 3 for x in a])
-    [4, 5, 6]
-    >>> print("a\n\nb")
-    a
-    <BLANKLINE>
-    b
-
-    The examples in docstrings can be run with the 'doctest' package:
-
-    python -m doctest -v doctest_simple_with_docs.py
-
-    See:
-
-    https://pymotw.com/2/doctest/
-
-    for more examples.  There are several different testing packages,
-    including 'doctest' and 'nose'.  All use the same format when it
-    comes to tests in docstrings, but offer different testing
-    functionality.
-
-    nosetests --with-doctest your_python_file.py
-
-    If your example prints random numbers, use an ellipsis ('...') to
-    skip the part of the output that is variable.
-
+"""
     Revisions
     ---------
     2026-09-15 ma755497@ucf.edu edited Revisions section, to note that this
                file was made a copy and is edited
-   """
+"""
 
 
     pass
